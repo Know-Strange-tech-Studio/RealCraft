8366789c05e4ee06c24799d7158089f6a1d46f6b
KnowScratcher <103301742+KnowScratcher@users.noreply.github.com>
2024-01-11 09:44:13 +0800

    update 1.0.5

--- com/ks/realcraft/Realcraft.java
+++ com/ks/realcraft/Realcraft.java
@@ -1,6 +1,8 @@
 package com.ks.realcraft;
 
-import com.ks.realcraft.craft.Craft;
+import com.ks.realcraft.craft.Crafting;
+import com.ks.realcraft.events.Instant_tnt_drop;
+import com.ks.realcraft.events.Instant_tnt_place;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,13 +14,22 @@ public class Realcraft extends JavaPlugin {
         saveDefaultConfig();
 
         getServer().getConsoleSender().sendMessage(ChatColor.LIGHT_PURPLE+"Adding Recipes");
-        Craft.init(getConfig());
 
+        Crafting.init(getConfig());
         /*getServer().getConsoleSender().sendMessage(ChatColor.LIGHT_PURPLE+"Adding Command");
         getCommand("setspawn").setExecutor(new SetWorldSpawn(this));
         getCommand("spawn").setExecutor(new Spawn(this));*/
+        getServer().getConsoleSender().sendMessage(ChatColor.LIGHT_PURPLE+"Done Adding Recipes");
 
-        getServer().getConsoleSender().sendMessage(ChatColor.LIGHT_PURPLE+"DONE Adding Recipes");
+
+        getServer().getConsoleSender().sendMessage(ChatColor.LIGHT_PURPLE+"Adding Event");
+
+        getServer().getPluginManager().registerEvents(new Instant_tnt_place(),this);
+        getServer().getPluginManager().registerEvents(new Instant_tnt_drop(),this);
+
+        getServer().getConsoleSender().sendMessage(ChatColor.LIGHT_PURPLE+"Done Adding Event");
+
+
         getServer().getConsoleSender().sendMessage(ChatColor.LIGHT_PURPLE + "[RealCraft] Real Craft is enabled.");
         getServer().getConsoleSender().sendMessage(ChatColor.LIGHT_PURPLE + "[RealCraft]-----------------------");
         getServer().getConsoleSender().sendMessage(ChatColor.LIGHT_PURPLE + "[RealCraft] |▔▔▔▔| |▔▔▔▔▔");
---
+++ com/ks/realcraft/craft/ItemControl.java
@@ -0,0 +1,21 @@
+package com.ks.realcraft.craft;
+
+import org.bukkit.inventory.ItemStack;
+
+public class ItemControl{
+
+    public static ItemStack grass_ash;
+    public static ItemStack potassium_nitrate_solution;
+    public static ItemStack potassium_nitrate;
+    public static ItemStack sulfur;
+    public static ItemStack hydrogen_peroxide;
+    public static ItemStack sulfur_dioxide;
+    public static ItemStack sulfuric_acid;
+    public static ItemStack nitric_acid;
+    public static ItemStack unstable_tnt;
+    public static ItemStack fat;
+    public static ItemStack production_selector;
+    public static ItemStack glycerol;
+    public static ItemStack calcium_carbonate;
+    public static ItemStack salt;
+}
